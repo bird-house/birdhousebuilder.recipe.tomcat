e2e3966ac93dc26752e5be6cf16358a6184c924c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-06-30 15:03:07 +0200

    installs tomcat with conda and setups supervisor start script

--- birdhousebuilder/recipe/tomcat/__init__.py
+++ birdhousebuilder/recipe/tomcat/__init__.py
@@ -9,7 +9,7 @@ from mako.template import Template
 import zc.buildout
 from birdhousebuilder.recipe import conda, supervisor
 
-templ_config = Template(filename=os.path.join(os.path.dirname(__file__), "nginx.conf"))
+#templ_config = Template(filename=os.path.join(os.path.dirname(__file__), "nginx.conf"))
 
 class Recipe(object):
     """This recipe is used by zc.buildout"""
@@ -20,90 +20,29 @@ class Recipe(object):
         self.anaconda_home = b_options.get('anaconda-home', conda.anaconda_home)
         self.options['prefix'] = self.anaconda_home
 
-        self.ssl_subject = options.get('ssl_subject', "/C=DE/ST=Hamburg/L=Hamburg/O=Phoenix/CN=localhost")
-        self.ssl_overwrite = options.query_bool('ssl_overwrite', default='false')
-        self.input = options.get('input')
-        self.sites = options.get('sites', name)
-
     def install(self):
         installed = []
-        installed += list(self.install_nginx())
-        installed += list(self.install_config())
-        installed += list(self.install_cert())
+        installed += list(self.install_tomcat())
         installed += list(self.install_program())
-        installed += list(self.install_sites())
         return installed
 
-    def install_nginx(self):
+    def install_tomcat(self):
         script = conda.Recipe(
             self.buildout,
             self.name,
-            {'pkgs': 'nginx'})
+            {'pkgs': 'apache-tomcat'})
 
-        conda.makedirs( os.path.join(self.anaconda_home, 'etc', 'nginx') )
-        conda.makedirs( os.path.join(self.anaconda_home, 'var', 'cache', 'nginx') )
-        conda.makedirs( os.path.join(self.anaconda_home, 'var', 'log', 'nginx') )
-        
         return script.install()
         
-    def install_config(self):
-        """
-        install nginx main config file
-        """
-        result = templ_config.render(
-            prefix=self.anaconda_home,
-            )
-
-        output = os.path.join(self.anaconda_home, 'etc', 'nginx', 'nginx.conf')
-        conda.makedirs(os.path.dirname(output))
-        
-        try:
-            os.remove(output)
-        except OSError:
-            pass
-
-        with open(output, 'wt') as fp:
-            fp.write(result)
-        return [output]
-
-    def install_cert(self):
-        from subprocess import check_call
-
-        cert = os.path.join(self.anaconda_home, "etc", "nginx", "phoenix.cert")
-        if not os.path.exists(cert) or self.ssl_overwrite:
-            cmd = templ_mkcert_script.render(
-                cert=cert,
-                ssl_subject=self.ssl_subject,
-                )
-            check_call(cmd, shell=True)
-            return [cert]
-        return []
-
     def install_program(self):
         script = supervisor.Recipe(
             self.buildout,
             self.name,
-            {'program': 'nginx',
-             'command': '%s/bin/nginx -c %s/etc/nginx/nginx.conf -g "daemon off;"' % (self.anaconda_home, self.anaconda_home),
+            {'program': 'tomcat',
+             'command': '%s/bin/catalina.sh run' % (self.anaconda_home),
              })
         return script.install()
 
-    def install_sites(self):
-        templ_sites = Template(filename=self.input)
-        result = templ_sites.render(**self.options)
-
-        output = os.path.join(self.anaconda_home, 'etc', 'nginx', 'conf.d', self.sites + '.conf')
-        conda.makedirs(os.path.dirname(output))
-        
-        try:
-            os.remove(output)
-        except OSError:
-            pass
-
-        with open(output, 'wt') as fp:
-            fp.write(result)
-        return [output]
-    
     def update(self):
         return self.install()
 
