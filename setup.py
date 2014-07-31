0b6dcba0a399101eecc9f49a3ed06ee30511c00d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-07-31 19:28:18 +0200

    release 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of birdhousebuilder.recipe.tomcat
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.1.1'
+version = '0.1.2'
 description = 'A Buildout recipe to install and configure tomcat'
 long_description = (
     open('README.rst').read() + '\n' +
