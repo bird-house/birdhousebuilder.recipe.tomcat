d4afb06c82fec183021828c94d92bc00a908a22a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-07-31 19:33:50 +0200

    update docs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 version = '0.1.2'
-description = 'A Buildout recipe to install and configure tomcat'
+description = 'A Buildout recipe to install and configure tomcat for Anaconda.'
 long_description = (
     open('README.rst').read() + '\n' +
     open('AUTHORS.rst').read() + '\n' +
