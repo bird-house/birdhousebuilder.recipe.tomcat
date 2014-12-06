7379392cff712091f4f8ef558111a47b26449a77
Carsten Ehbrecht <carsten@linacs.org>
2014-12-06 15:34:14 +0100

    release 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of birdhousebuilder.recipe.tomcat
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.1.3'
+version = '0.1.4'
 description = 'A Buildout recipe to install and configure tomcat for Anaconda.'
 long_description = (
     open('README.rst').read() + '\n' +
