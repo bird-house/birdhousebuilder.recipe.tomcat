e4f7fab0eb68a49744a7bb6f91c6d3ed0212c03c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-12-13 13:16:46 +0100

    pep8

--- birdhousebuilder/recipe/tomcat/__init__.py
+++ birdhousebuilder/recipe/tomcat/__init__.py
@@ -20,6 +20,7 @@ tomcat_users_xml = Template(filename=os.path.join(os.path.dirname(__file__), "to
 server_xml = Template(filename=os.path.join(os.path.dirname(__file__), "server.xml"))
 logging_props = Template(filename=os.path.join(os.path.dirname(__file__), "logging.properties"))
 
+
 def unzip(prefix, warfile):
     warname = os.path.basename(warfile)
     dirname = warname[0:-4]
@@ -32,11 +33,13 @@ def unzip(prefix, warfile):
         except:
             raise
 
+
 def make_dirs(name, user, mode=0o755):
     etc_uid, etc_gid = pwd.getpwnam(user)[2:4]
     created = []
     make_dir(name, etc_uid, etc_gid, mode, created)
-        
+
+
 class Recipe(object):
     """This recipe is used by zc.buildout.
     It installs apache-tomcat as conda package and setups tomcat configuration"""
@@ -55,7 +58,7 @@ class Recipe(object):
             if section_name in buildout._raw:
                 raise KeyError("already in buildout", section_name)
             buildout._raw[section_name] = options
-            buildout[section_name] # cause it to be added to the working parts
+            buildout[section_name]  # cause it to be added to the working parts
 
         self.deployment_name = self.name + "-tomcat-deployment"
         self.deployment = zc.recipe.deployment.Install(buildout, self.deployment_name, {
@@ -64,7 +67,7 @@ class Recipe(object):
             'user': self.options['user'],
             'etc-user': self.options['etc-user']})
         add_section(self.deployment_name, self.deployment.options)
-        
+
         self.options['etc-prefix'] = self.options['etc_prefix'] = self.deployment.options['etc-prefix']
         self.options['var-prefix'] = self.options['var_prefix'] = self.deployment.options['var-prefix']
         self.options['etc-directory'] = self.options['etc_directory'] = self.deployment.options['etc-directory']
@@ -81,7 +84,7 @@ class Recipe(object):
         self.conda = birdhousebuilder.recipe.conda.Recipe(self.buildout, self.name, {
             'env': self.options['env'],
             'pkgs': self.options['pkgs'],
-            'channels': self.options['channels'] })
+            'channels': self.options['channels']})
         self.options['conda-prefix'] = self.options['conda_prefix'] = self.conda.options['prefix']
 
         # tomcat options
@@ -91,7 +94,8 @@ class Recipe(object):
         self.options['catalina_base'] = self.options['catalina-base'] = self.options['lib-directory']
 
         # java options
-        self.options['java_home'] = self.options['java-home'] = self.options.get('java-home', self.options['conda-prefix'])
+        self.options['java_home'] = self.options['java-home'] = \
+            self.options.get('java-home', self.options['conda-prefix'])
         self.options['Xmx'] = self.options.get('Xmx', '1024m')
         self.options['Xms'] = self.options.get('Xms', '128m')
         self.options['MaxPermSize'] = self.options.get('MaxPermSize', '128m')
@@ -102,18 +106,21 @@ class Recipe(object):
         self.options['ncwms_password'] = self.options.get('ncwms_password', '')
 
         # make folders
-        make_dirs(os.path.join(self.options['catalina-base'], 'bin' ), self.options['etc-user'], mode=0o755)
-        make_dirs(os.path.join(self.options['catalina-base'], 'conf' ), self.options['etc-user'], mode=0o755)
-        make_dirs(os.path.join(self.options['catalina-base'], 'logs' ), self.options['user'], mode=0o755)
-        make_dirs(os.path.join(self.options['catalina-base'], 'temp' ), self.options['user'], mode=0o755)
-        make_dirs(os.path.join(self.options['catalina-base'], 'webapps' ), self.options['user'], mode=0o755)
-        make_dirs(os.path.join(self.options['catalina-base'], 'work' ), self.options['user'], mode=0o755)
+        make_dirs(os.path.join(self.options['catalina-base'], 'bin'), self.options['etc-user'], mode=0o755)
+        make_dirs(os.path.join(self.options['catalina-base'], 'conf'), self.options['etc-user'], mode=0o755)
+        make_dirs(os.path.join(self.options['catalina-base'], 'logs'), self.options['user'], mode=0o755)
+        make_dirs(os.path.join(self.options['catalina-base'], 'temp'), self.options['user'], mode=0o755)
+        make_dirs(os.path.join(self.options['catalina-base'], 'webapps'), self.options['user'], mode=0o755)
+        make_dirs(os.path.join(self.options['catalina-base'], 'work'), self.options['user'], mode=0o755)
 
         # prepare java prefs folders
         # http://stackoverflow.com/questions/15004954/java-setting-preferences-backingstore-directory
-        make_dirs(os.path.join(self.options['catalina-base'], "java_prefs", ".java"), self.options['user'], mode=0o755)
-        make_dirs(os.path.join(self.options['catalina-base'], "java_prefs", ".java", ".systemPrefs"), self.options['user'], mode=0o755)
-        make_dirs(os.path.join(self.options['catalina-base'], "java_prefs", ".java", ".userPrefs"), self.options['user'], mode=0o755)
+        make_dirs(os.path.join(self.options['catalina-base'], "java_prefs", ".java"),
+                  self.options['user'], mode=0o755)
+        make_dirs(os.path.join(self.options['catalina-base'], "java_prefs", ".java", ".systemPrefs"),
+                  self.options['user'], mode=0o755)
+        make_dirs(os.path.join(self.options['catalina-base'], "java_prefs", ".java", ".userPrefs"),
+                  self.options['user'], mode=0o755)
 
     def install(self, update=False):
         installed = []
@@ -132,7 +139,7 @@ class Recipe(object):
     def install_catalina_sh(self):
         config = Configuration(self.buildout, 'catalina.sh', {
             'deployment': self.deployment_name,
-            'directory': os.path.join(self.options['catalina-base'], 'bin' ),
+            'directory': os.path.join(self.options['catalina-base'], 'bin'),
             'mode': '0o755',
             'file': os.path.join(self.options['catalina-home'], 'bin', 'catalina.sh')})
         installed = [config.install()]
@@ -144,14 +151,14 @@ class Recipe(object):
         text = setenv_sh.render(**self.options)
         config = Configuration(self.buildout, 'setenv.sh', {
             'deployment': self.deployment_name,
-            'directory': os.path.join(self.options['catalina-base'], 'bin' ),
+            'directory': os.path.join(self.options['catalina-base'], 'bin'),
             'text': text})
         return [config.install()]
 
     def install_web_xml(self):
         config = Configuration(self.buildout, 'web.xml', {
             'deployment': self.deployment_name,
-            'directory': os.path.join(self.options['catalina-base'], 'conf' ),
+            'directory': os.path.join(self.options['catalina-base'], 'conf'),
             'file': os.path.join(self.options['catalina-home'], 'conf', 'web.xml')})
         return [config.install()]
 
@@ -159,7 +166,7 @@ class Recipe(object):
         text = tomcat_users_xml.render(**self.options)
         config = Configuration(self.buildout, 'tomcat-users.xml', {
             'deployment': self.deployment_name,
-            'directory': os.path.join(self.options['catalina-base'], 'conf' ),
+            'directory': os.path.join(self.options['catalina-base'], 'conf'),
             'text': text})
         return [config.install()]
 
@@ -167,7 +174,7 @@ class Recipe(object):
         text = server_xml.render(**self.options)
         config = Configuration(self.buildout, 'server.xml', {
             'deployment': self.deployment_name,
-            'directory': os.path.join(self.options['catalina-base'], 'conf' ),
+            'directory': os.path.join(self.options['catalina-base'], 'conf'),
             'text': text})
         return [config.install()]
 
@@ -175,10 +182,10 @@ class Recipe(object):
         text = logging_props.render(**self.options)
         config = Configuration(self.buildout, 'logging.properties', {
             'deployment': self.deployment_name,
-            'directory': os.path.join(self.options['catalina-base'], 'conf' ),
+            'directory': os.path.join(self.options['catalina-base'], 'conf'),
             'text': text})
         return [config.install()]
-    
+
     def install_supervisor(self, update=False):
         script = supervisor.Recipe(
             self.buildout,
@@ -187,13 +194,13 @@ class Recipe(object):
              'user': self.options.get('user'),
              'etc-user': self.options['etc-user'],
              'program': 'tomcat',
-             'command':  '{0} run'.format(os.path.join(self.options['catalina-base'], 'bin', 'catalina.sh')),
+             'command': '{0} run'.format(os.path.join(self.options['catalina-base'], 'bin', 'catalina.sh')),
              })
         return script.install(update)
 
     def update(self):
         return self.install(update=True)
 
+
 def uninstall(name, options):
     pass
-
